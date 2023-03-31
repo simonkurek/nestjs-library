0ba2d996c83d60f24758a44e65b9cfad8897687e
Simon Kurek <simonkurek@protonmail.com>
2023-03-31 23:16:00 +0200

    feat: db added in docker-compose; change: default db name in db.config.ts

--- libs/api/utils-config/src/lib/db.config.ts
+++ libs/api/utils-config/src/lib/db.config.ts
@@ -7,7 +7,7 @@ export const databaseConfiguration = registerAs('database', () => {
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'pass123',
-    database: process.env.DB_NAME || 'app',
+    database: process.env.DB_NAME || 'postgres',
   };
 });
 
