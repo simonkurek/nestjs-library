23d101bf812c53401276615f1fabbea9d12b0caa
Simon Kurek <simonkurek@protonmail.com>
2023-03-31 23:11:27 +0200

    feat: typeorm-config

--- apps/api/src/app/app-core.module.ts
+++ apps/api/src/app/app-core.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiFeatureConfigModule } from '@nestjs-library/api/feature-config';
+import { databaseConfiguration } from '@nestjs-library/api/utils-config';
 
-@Module({})
+@Module({
+  imports: [
+    ApiFeatureConfigModule,
+    TypeOrmModule.forRootAsync({
+      inject: [databaseConfiguration.KEY],
+      useFactory: (config) => ({
+        type: 'postgres',
+        host: config.host,
+        port: config.port,
+        username: config.username,
+        password: config.password,
+        database: config.database,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+})
 export class AppCoreModule {}
